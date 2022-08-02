206b63eb94f3a53f9a3a8cd8ebef0fbe4e0876ad
hussain ali <hussain.a@otomeyt.ai>
2022-08-02 18:08:23 +0530

    Update project.js

--- components/solution/project.js
+++ components/solution/project.js
@@ -7,8 +7,9 @@ function Project(props) {
             <div className=''>
                 <div className='lg:flex items-center'>
                     <div className='lg:w-[50%] pl-10 pt-8 pb-8'>
-                         <h3 className='text-5xl '>some of our <br/> industry-specific <br/> projects</h3>
-                         <p className='pt-3 color-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                         <h3 className='text-5xl '>some of our <br/> industry-specific <br/> projects</h3>      
+
+                         <p className='pt-3 color-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                          <button className='bg-white h-10 px-6 mt-8 Gilroy-Bold text-base text-black'>see our concept</button>
                     </div>
                 </div>
