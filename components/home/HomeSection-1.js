bb9881a7a1e7287d66b2ff37a95d6896114d6d24
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2023-02-20 18:32:03 +0530

    test commit

--- components/home/HomeSection-1.js
+++ components/home/HomeSection-1.js
@@ -9,17 +9,17 @@ const HomeSectionOne = () => {
         <div className='lg:w-[40%] w-full  md:h-full'>
           <div className='3xl:py-10 2xl:py-6 xl:py-6 2xl:px-14 xl:px-12 px-10 flex flex-col justify-center 2xl:space-y-5 xl:space-y-4 space-y-3 h-full'>
           
-            <div className='2xl:text-2xl lg:text-xl text-lg Gilroy-Bold color-gray'>We are__</div>
+            <div className='2xl:text-3xl lg:text-3xl text-lg Gilroy-Bold color-gray'>We are__</div>
         
-            <div className='flex flex-col'>
-              <div className='text-heading-4'>a digital production company focusing heavily on design & development based in indore. we create products that make people say oh damn!</div>
+            <div className='flex flex-col w-full px-12 py-4'>
+              <div className='text-heading-4 tracking-widest text-2xl'>a digital production company focusing heavily on design & development based in indore. we create products that make people say oh damn!</div>
             </div>
             <div className="rotating scroll-logo md:block hidden">
               <img src="/images/scrollAnimation.svg"></img>
            </div>
           </div>
         </div>
-        <div className='md:w-[60%] w-full  md:h-full mt-14 md:mt-0'>
+        <div className='md:w-[45%] w-full  md:h-full mt-14 md:mt-0'>
           <div className="3xl:py-10 2xl:py-6 xl:py-6 2xl:px-14 xl:px-12 px-10 ">
           <video src="/videos/FinalVideo.mp4" className="relative md:top-[3rem] top-0"  muted loop controls={false} autoPlay={true} />
           </div>
